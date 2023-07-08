f17a04e89f452840c613c49ecb9cc69ad0ee6aef
isalay1003 <113488344+isalay1003@users.noreply.github.com>
2023-07-08 11:22:13 +0800

    Create Grove_practice.ino

---
+++ Grove_practice.ino
@@ -0,0 +1,50 @@
+#include "DHT.h"
+DHT dht11_p3(3, DHT11);
+
+#include <Arduino.h>
+#include <U8x8lib.h>
+U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+
+int rotary = 0;
+int LED = 4;
+int button = 6;
+int lightSensor = A6;
+int soundSensor = A2;
+
+float temp;
+float humi;
+int volume;
+int bright;
+int val;
+
+void setup(){
+  u8x8.begin();
+  u8x8.setFlipMode(1);
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  // set LED and button pinMode
+  Serial.begin(9600);
+}
+
+
+void loop(){
+  // check if the button is pressed
+  if(digitalRead(button) == HIGH){     //button pressed
+    val = analogRead(rotary);
+    u8x8.clearDisplay();
+  }
+  Serial.println(val);
+  u8x8.setCursor(0, 33);
+  if(val < 256){
+    // temp mode 
+  }
+  else if(val < 512){
+    // humi mode
+  }
+  else if(val < 768){
+    // brightness mode
+  }
+  else{
+    // volume mode
+  }
+  delay(200);
+}
